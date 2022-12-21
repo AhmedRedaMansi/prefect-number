f4bf524cf4c5ca6b204b782355bc54a6355c636f
AhmedRedaMansi <121058536+AhmedRedaMansi@users.noreply.github.com>
2022-12-21 21:52:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1, num2;
+int sum = 0;
+Console.WriteLine("first number:");
+
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("last number:");
+
+num2 = int.Parse(Console.ReadLine());
+
+Console.Write("numbers found: ");
+for (int i = num1; i < num2; i++)
+{
+
+    for (int timer = 1; timer < i; timer++)
+    {
+
+        if (i % timer == 0) sum += timer;
+    }
+    if (sum == i)
+    {
+
+        Console.Write(i + ", ");
+
+    }
+
+    sum = 0;
+}   
+    
+
